086b630b250ee131bd64dfd34c509d2f0460fdb5
Ksieon <keumsiun0503@gmail.com>
2025-05-15 22:07:00 +0900

    :recycle:Refactor: 전역 에러 통합

--- src/error.js
+++ src/error.js
@@ -7,3 +7,43 @@ export class DuplicateUserEmailError extends Error {
         this.data = data;
     }
 }
+
+export class DuplicateRegionError extends Error {
+    errorCode = "U002";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+    }
+}
+
+export class DuplicateStoreError extends Error {
+    errorCode = "U003";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+    }
+}
+
+export class NoExistStoreError extends Error {
+    errorCode = "U004";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+    }
+}
+
+export class DuplicateReviewError extends Error {
+    errorCode = "U005";
+
+    constructor(reason, data) {
+        super(reason);
+        this.reason = reason;
+        this.data = data;
+    }
+}
