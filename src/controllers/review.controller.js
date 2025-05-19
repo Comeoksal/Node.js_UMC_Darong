dbb2834ca1648674998f4364e2a1e4802d3c80a5
Sieon Keum <keumsiun0503@gmail.com>
2025-05-20 01:38:13 +0900

    ♻️Refactor: swagger 주석 추가 및 리팩토링 (#23)
    * :wrench:Settings: dependencies setting
    
    * :sparkles:Feat: week8 work
    
    * :recycle:Refactor: week8 update
    
    * :recycle:Refactor: swagger 주석 추가 및 리팩토링
    
    * :recycle:Refactor: swagger 명세 작성(에러 코드 반영)

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -7,5 +7,88 @@ export const handleReview = async (req, res, next) => {
     console.log('body : ', req.body);
 
     const review = await UseReview(bodyToReview(req.body));
+    // #region Swagger: 리뷰 추가 API
+    /*
+      #swagger.summary = '리뷰 추가 API';
+      #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                name: { type: "string" },
+              }
+            }
+          }
+        }
+      };
+      #swagger.responses[200] = {
+        description: "리뷰 추가 성공 응답",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                resultType: { type: "string", example: "SUCCESS" },
+                error: { type: "object", nullable: true, example: null },
+                success: {
+                  type: "object", 
+                  properties: {
+                  body: { type: "string", example: "리뷰 내용~~" },
+                  score: {type : "float", example: "4.3" },
+                }
+                }
+              }
+            }
+          }
+        }
+      };
+      #swagger.responses[400] = {
+        description: "리뷰 추가 실패 응답",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: { 
+                resultType: { type: "string", example: "FAIL" },
+                error: {
+                  type: "object",
+                  properties: {
+                    errorCode: { type: "string", example: "400" },
+                    reason: { type: "string" },
+                    data: { type: "object" }
+                  }
+                },
+                success: { type: "object", nullable: true, example: null }
+              }
+            }
+          }
+        }
+      };
+      #swagger.responses[409] = {
+      description: "동일 리뷰 작성 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U005" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    */
+    // #endregion
     res.status(StatusCodes.OK).success(review);
 }
--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -4,138 +4,182 @@ import { UseStore } from '../services/store.service.js';
 import { GetStore } from '../services/store.service.js';
 
 export const handleStore = async (req, res, next) => {
-    console.log("가게 추가를 요청했습니다.");
-    console.log("body : ", req.body);
+  console.log("가게 추가를 요청했습니다.");
+  console.log("body : ", req.body);
 
-    const store = await UseStore(bodyToStore(req.body));
-    // #region Swagger: 가게 추가 API
-    /*
-      #swagger.summary = '가게 추가 API';
-      #swagger.requestBody = {
-        required: true,
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                region_id: { type: "bigint" },
-                name: { type: "string" },
-                address: { type: "string" },
+  const store = await UseStore(bodyToStore(req.body));
+  // #region Swagger: 가게 추가 API
+  /*
+    #swagger.summary = '가게 추가 API';
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              region_id: { type: "bigint" },
+              name: { type: "string" },
+              address: { type: "string" },
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[200] = {
+      description: "가게 추가 성공 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "SUCCESS" },
+              error: { type: "object", nullable: true, example: null },
+              success: {
+                type: "string", example: "store save complete"
               }
             }
           }
         }
-      };
-      #swagger.responses[200] = {
-        description: "가게 추가 성공 응답",
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                resultType: { type: "string", example: "SUCCESS" },
-                error: { type: "object", nullable: true, example: null },
-                success: {
-                  type: "string", example: "store save complete"
+      }
+    };
+    #swagger.responses[400] = {
+      description: "가게 추가 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: { 
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "400" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
                 }
-              }
+              },
+              success: { type: "object", nullable: true, example: null }
             }
           }
         }
-      };
-      #swagger.responses[400] = {
-        description: "가게 추가 실패 응답",
-        content: {
-          "application/json": {
-            schema: {
+      }
+    };
+    #swagger.responses[409] = {
+    description: "가게 중복 응답",
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            resultType: { type: "string", example: "FAIL" },
+            error: {
               type: "object",
-              properties: { 
-                resultType: { type: "string", example: "FAIL" },
-                error: {
-                  type: "object",
-                  properties: {
-                    errorCode: { type: "string", example: "U001" },
-                    reason: { type: "string" },
-                    data: { type: "object" }
-                  }
-                },
-                success: { type: "object", nullable: true, example: null }
+              properties: {
+                errorCode: { type: "string", example: "U003" },
+                reason: { type: "string" },
+                data: { type: "object" }
               }
-            }
+            },
+            success: { type: "object", nullable: true, example: null }
           }
         }
-      };
-    */
-    // #endregion
-    res.status(StatusCodes.OK).success(store);
+      }
+    }
+  };
+  */
+  // #endregion
+  res.status(StatusCodes.OK).success(store);
 }
 
 export const getStoreInfo = async (req, res, next) => {
-    console.log("가게 조회를 요청했습니다.");
-    console.log("body : ", req.body);
+  console.log("가게 조회를 요청했습니다.");
+  console.log("body : ", req.body);
 
-    const store = await GetStore(bodyToStore_2(req.body));
-    // #region Swagger: 가게 조회 API
-    /*
-      #swagger.summary = '가게 조회 API';
-      #swagger.requestBody = {
-        required: true,
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                id:  {type: "bigint"},
-              }
+  const store = await GetStore(bodyToStore_2(req.body));
+  // #region Swagger: 가게 조회 API
+  /*
+    #swagger.summary = '가게 조회 API';
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              id:  {type: "bigint"},
             }
           }
         }
-      };
-      #swagger.responses[200] = {
-        description: "가게 조회 성공 응답",
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                resultType: { type: "string", example: "SUCCESS" },
-                error: { type: "object", nullable: true, example: null },
-                success: {
-                  type: "object",
-                  properties: {
-                    name: { type: "string" },
-                    address: { type: "string" },
-                    score: {type: "float"},
-                  }
+      }
+    };
+    #swagger.responses[200] = {
+      description: "가게 조회 성공 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "SUCCESS" },
+              error: { type: "object", nullable: true, example: null },
+              success: {
+                type: "object",
+                properties: {
+                  name: { type: "string" },
+                  address: { type: "string" },
+                  score: {type: "float"},
                 }
               }
             }
           }
         }
-      };
-      #swagger.responses[400] = {
-        description: "회원 가입 실패 응답",
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                resultType: { type: "string", example: "FAIL" },
-                error: {
-                  type: "object",
-                  properties: {
-                    errorCode: { type: "string", example: "U001" },
-                    reason: { type: "string" },
-                    data: { type: "object" }
-                  }
-                },
-                success: { type: "object", nullable: true, example: null }
-              }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "가게 조회 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "400" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
             }
           }
         }
-      };
-    */
-    // #endregion
-    res.status(StatusCodes.OK).success(store);
+      }
+    };
+    #swagger.responses[404] = {
+    description: "존재하지 않는 가게 조회 응답",
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            resultType: { type: "string", example: "FAIL" },
+            error: {
+              type: "object",
+              properties: {
+                errorCode: { type: "string", example: "U004" },
+                reason: { type: "string" },
+                data: { type: "object" }
+              }
+            },
+            success: { type: "object", nullable: true, example: null }
+          }
+        }
+      }
+    }
+  };
+  */
+  // #endregion
+  res.status(StatusCodes.OK).success(store);
 }
